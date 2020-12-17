d296d363516294d0b636aa96de0c4bc0999ca198
azgmus <herodotus@inbox.ru>
2020-12-17 22:52:41 +0300

    1 hour of tutorial(list[remove, append, del, pop])

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-list1 = ["pizza", "cheese", "burger"]
+from collections import Counter
+
+
+list1 = ["pizza", "burger", "burger",
+         "pizza", "cheese", "burger",
+         "pizza", "cheese", "burger"]
+
+list1.insert(3, "inserted Value")
+
+
+while list1.count("pizza"):
+    list1.remove("pizza")
+print(Counter(list1))
